89fbccc01ca5b48d2e5ad75a688c07ecd226cf02
Celestigrr <iannyenze345@gmail.com>
2026-09-15 15:42:33 +0300

    updated

--- pizza-inn-react/src/components/deals.jsx
+++ pizza-inn-react/src/components/deals.jsx
@@ -4,13 +4,31 @@ function Deals() {
   return (
     <section className="deals">
       <h2>WEEKLY SPECIAL</h2>
+
       <p>FRENZY TUESDAY & THURSDAYS</p>
+
+      <p>
+        BUY <span className="one">ONE</span> GET{" "}
+        <span className="one">ONE</span> FREE
+      </p>
 
       <div className="deals-products-prices">
         <PizzaCard
           name="Hawaiian Pizza"
           price="1300"
           image="/images/hawaiian-pizza.jpg"
+        />
+
+        <PizzaCard
+          name="Pepperoni Pizza"
+          price="1100"
+          image="/images/pepperoni-pizza.jpg"
+        />
+
+        <PizzaCard
+          name="Supreme Pizza"
+          price="1600"
+          image="/images/supreme-pizza.jpg"
         />
       </div>
     </section>
--- pizza-inn-react/src/components/pizzacard.jsx
+++ pizza-inn-react/src/components/pizzacard.jsx
@@ -3,8 +3,13 @@ function PizzaCard({ name, price, image }) {
     <div className="deals-product">
       <img src={image} alt={name} />
 
-      <p>{name}</p>
-      <p>KES {price}</p>
+      <div className="pizza-info">
+        <p>{name}</p>
+
+        <div className="pizza-price-row">
+          <p>KES {price}</p>
+        </div>
+      </div>
     </div>
   );
 }
